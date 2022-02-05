c8050e562050d3edbdaa39668a6128ae5ad00d57
Ahmad Almabhoh <az540546@gmail.com>
2022-02-05 12:55:30 +0200

    User-CRUD

---
+++ app/Http/Controllers/RolePermissionsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
+
+class RolePermissionsController extends Controller
+{
+    //
+}
--- database/seeders/PermissionSeeder.php
+++ database/seeders/PermissionSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 
 class PermissionSeeder extends Seeder
 {
@@ -14,5 +15,30 @@ class PermissionSeeder extends Seeder
     public function run()
     {
         //
+
+        $permissions = [
+            'Show-Admin',
+            'Update-Admin',
+            'Add-Admin',
+            'Delete-Admin',
+            'Show-User',
+            'Update-User',
+            'Delete-User',
+            'Add-User',
+            'Show-Category',
+            'Update-Category',
+            'Delete-Category',
+            'Add-Category',
+            'Show-City',
+            'Update-City',
+            'Delete-City',
+            'Add-City',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create([
+                'name' => $permission
+            ]);
+        }
     }
 }
